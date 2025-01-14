d4400587842cc922963f3650b491e7a2482574c7
Divya <divyabaheti101@gmail.com>
2025-01-14 12:11:14 +0530

    setup form and connectButton from rainbow kit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 import './App.css';
+import EtherWallet from './artifacts/contracts/EtherWallet.sol/EtherWallet.json'
+import { useState } from 'react';
+import { FormControl } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function App() {
+  const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+
+  //EtherWallet Smart Contract handling
+  const [scBalance, setScBalnce] = useState(0)
+  const [ethToUseForDeposit, setEthToUseForDeposit] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container flex flex-col items-center mt-10'>
+      <div className='flex-mb-6'>
+        <ConnectButton />
+      </div>
+      <h3 className='text=5x1 font-bold mb-20'>
+        {"Deposit to Ether Wallet Smart Contract"}
+      </h3>
+
+      <Form>
+              <Form.Group className='mb-3' controlID='numberInEth'>
+                <FormControl type='text' placeholder='Amount in ETH'
+                  //onChange={(e) => setEthToUseForDeposit(e.target.value)} 
+                  />
+              </Form.Group>
+              <Button variant='primary' 
+              //onClick={depositToEtherWalletContract} 
+              >
+                Deposit to Ether Wallet Smart Contract
+              </Button>
+            </Form>
+
+            <div>Ether Wallet Smart Contract Address: {contractAddress}</div>
+          <div>Ether Wallet Smart Contract Balance: {scBalance}</div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { configureChains, createClient, WagmiConfig, chain } from 'wagmi';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
@@ -46,4 +47,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
