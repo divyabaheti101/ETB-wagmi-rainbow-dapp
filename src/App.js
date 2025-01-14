d622df768f07c8b89597efaf578285274db5d827
Divya <divyabaheti101@gmail.com>
2025-01-14 16:33:02 +0530

    withdraw functionality

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import { FormControl } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { useContract, useContractRead, useSigner } from 'wagmi';
+import { useContract, useContractRead, useContractWrite, useSigner } from 'wagmi';
 import { ethers } from 'ethers';
 
 function App() {
@@ -14,6 +14,8 @@ function App() {
   //EtherWallet Smart Contract handling
   const [scBalance, setScBalnce] = useState(0)
   const [ethToUseForDeposit, setEthToUseForDeposit] = useState(0)
+  const [ethToUseForWithdraw, setEthToUseForWithdraw] = useState(0)
+  const [receiverAddress, setReceiverAddress] = useState(ethers.constants.AddressZero)
 
   const { data: contractBalance} = useContractRead({
     addressOrName: contractAddress,
@@ -24,21 +26,33 @@ function App() {
 
   useEffect(() => {
     if(contractBalance) {
-      let temp = contractBalance / 10**18
+      let temp = contractBalance / 10**15
       setScBalnce(temp)
     }
   }, [contractBalance])
 
   const {data: signer} = useSigner()
-  const depositETH = useContract({
+  const contract = useContract({
     addressOrName: contractAddress,
     contractInterface: EtherWallet.abi,
     signerOrProvider: signer
   })
   const depositToEtherWalletContract = async() => {
-    await depositETH.deposit({
+    await contract.deposit({
       value: ethers.utils.parseEther(ethToUseForDeposit)
     })
+  }
+
+  // const withdrawETH = useContractWrite({
+  //   addressOrName: contractAddress,
+  //   contractInterface: EtherWallet.abi,
+  //   functionName: 'withdraw',
+  //   args: (receiverAddress, ethToUseForWithdraw)
+  // })
+  const withdrawETHfromContract = async() => {
+    await contract.withdraw(receiverAddress, ethers.utils.parseEther(ethToUseForWithdraw))
+    setEthToUseForWithdraw(0)
+    setReceiverAddress(ethers.constants.AddressZero)
   }
 
 
@@ -64,8 +78,20 @@ function App() {
         </Button>
       </Form>
 
+      <Form>
+              <Form.Group className='mb-3' controlID='numberInEthtoWithdraw'>
+                <FormControl type='text' placeholder='Amount in ETH'
+                  onChange={(e) => setEthToUseForWithdraw(e.target.value)} />
+                <FormControl type='text' placeholder='Address to send to'
+                  onChange={(e) => setReceiverAddress(e.target.value)} />
+              </Form.Group>
+              <Button variant='primary' onClick={withdrawETHfromContract} >
+                Withdraw money from Wallet
+              </Button>
+            </Form>
+
       <div>Ether Wallet Smart Contract Address: {contractAddress}</div>
-      <div>Ether Wallet Smart Contract Balance: {scBalance}</div>
+      <div>Ether Wallet Smart Contract Balance: {scBalance} /1000 ETH</div>
     </div>
   );
 }
