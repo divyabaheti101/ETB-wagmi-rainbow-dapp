433b1581fd5b73f17f68613ae87789e1808ac175
Divya <divyabaheti101@gmail.com>
2025-01-14 12:37:31 +0530

    interacting with contract on hardhat using wagmi hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import './App.css';
 import EtherWallet from './artifacts/contracts/EtherWallet.sol/EtherWallet.json'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FormControl } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useContract, useContractRead, useSigner } from 'wagmi';
+import { ethers } from 'ethers';
 
 function App() {
   const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
@@ -12,6 +14,33 @@ function App() {
   //EtherWallet Smart Contract handling
   const [scBalance, setScBalnce] = useState(0)
   const [ethToUseForDeposit, setEthToUseForDeposit] = useState(0)
+
+  const { data: contractBalance} = useContractRead({
+    addressOrName: contractAddress,
+    contractInterface: EtherWallet.abi,
+    functionName: 'balanceOf',
+    watch: true
+  })
+
+  useEffect(() => {
+    if(contractBalance) {
+      let temp = contractBalance / 10**18
+      setScBalnce(temp)
+    }
+  }, [contractBalance])
+
+  const {data: signer} = useSigner()
+  const depositETH = useContract({
+    addressOrName: contractAddress,
+    contractInterface: EtherWallet.abi,
+    signerOrProvider: signer
+  })
+  const depositToEtherWalletContract = async() => {
+    await depositETH.deposit({
+      value: ethers.utils.parseEther(ethToUseForDeposit)
+    })
+  }
+
 
   return (
     <div className='container flex flex-col items-center mt-10'>
@@ -23,20 +52,20 @@ function App() {
       </h3>
 
       <Form>
-              <Form.Group className='mb-3' controlID='numberInEth'>
-                <FormControl type='text' placeholder='Amount in ETH'
-                  //onChange={(e) => setEthToUseForDeposit(e.target.value)} 
-                  />
-              </Form.Group>
-              <Button variant='primary' 
-              //onClick={depositToEtherWalletContract} 
-              >
-                Deposit to Ether Wallet Smart Contract
-              </Button>
-            </Form>
+        <Form.Group className='mb-3' controlID='numberInEth'>
+          <FormControl type='text' placeholder='Amount in ETH'
+            onChange={(e) => setEthToUseForDeposit(e.target.value)} 
+          />
+        </Form.Group>
+        <Button variant='primary' 
+          onClick={depositToEtherWalletContract} 
+        >
+          Deposit to Ether Wallet Smart Contract
+        </Button>
+      </Form>
 
-            <div>Ether Wallet Smart Contract Address: {contractAddress}</div>
-          <div>Ether Wallet Smart Contract Balance: {scBalance}</div>
+      <div>Ether Wallet Smart Contract Address: {contractAddress}</div>
+      <div>Ether Wallet Smart Contract Balance: {scBalance}</div>
     </div>
   );
 }
