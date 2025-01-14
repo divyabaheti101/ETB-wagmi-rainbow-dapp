802359ddeb70a4d5a9cadb36561c74ea86d11b3e
Divya <divyabaheti101@gmail.com>
2025-01-14 10:48:24 +0530

    hardhat and contract setups

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,37 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners()
+
+  for (const account of accounts) {
+    const balance = await account.getBalance()
+    console.log(account.address, ': ', balance);
+  }
+});
+
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async (taskArgs) => {
+    const balance = await ethers.provider.getBalance(taskArgs.account);
+
+    console.log(ethers.formatEther(balance), "ETH");
+});
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  paths: {
+    sources: './contracts',
+    artifacts: './src/artifacts'
+  },
+  defaultNetwork: 'hardhat',
+  networks:{
+    hardhat:{
+      chainId: 1337
+    },
+    goerli: {
+      url: 'https://sepolia.drpc.org'
+      //accounts
+    }
+  }
 };
---
+++ ignition/modules/EtherWallet.js
@@ -0,0 +1,8 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+module.exports = buildModule("EtherWalletModule", (m) => {
+  const etherWallet = m.contract("EtherWallet");
+  return { etherWallet };
+});
